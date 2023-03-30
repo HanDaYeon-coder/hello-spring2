fa03042e3dbf6ab0f85798b7ed7764341b240163
HanDaYeon-coder <hdy78901@naver.com>
2023-03-30 23:17:24 +0900

    Initial commit

---
+++ src/main/java/hello2/hellospring2/SpringConfig.java
@@ -0,0 +1,23 @@
+package hello2.hellospring2;
+
+import hello2.hellospring2.aop.TimeTraceAop;
+import hello2.hellospring2.repository.MemberRepository;
+import hello2.hellospring2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+}
